3ec7912055d0a18e5b93cbf159f18fe12553bce0
Jeremy Rocheleau <107440471+slamdunk061@users.noreply.github.com>
2024-02-25 17:08:25 -0500

    Update employee_details.sql
    Employee details with salary information

--- employee_details.sql
+++ employee_details.sql
@@ -1,1 +1,5 @@
 /* List the following details of each employee: employee number, last name, first name, gender, and salary. */
+
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;

2ec8b2cc67e9702c416063372255bb20b3100de2
Jeremy Rocheleau <107440471+slamdunk061@users.noreply.github.com>
2024-02-25 17:24:36 -0500

    Update emp_dept.sql
    Displays all employees information and department

--- emp_dept.sql
+++ emp_dept.sql
@@ -1,1 +1,5 @@
 /* List the department of each employee with the following information: employee number, last name, first name, and department name. */
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e 
+JOIN dept_manager m on m.emp_no = e.emp_no
+JOIN departments d ON d.dept_no = m.dept_no;

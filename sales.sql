7ae81896f14f86956e9fcfdeff710c1a93665b97
Jeremy Rocheleau <107440471+slamdunk061@users.noreply.github.com>
2024-02-25 17:32:44 -0500

    Update sales.sql
    Displays all employees in the Sales department

--- sales.sql
+++ sales.sql
@@ -1,1 +1,6 @@
 /* List all employees in the Sales department, including their employee number, last name, first name, and department name. */
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_manager m ON e.emp_no = m.emp_no
+JOIN departments d ON d.dept_no = m.dept_no
+WHERE d.dept_name = 'Sales';

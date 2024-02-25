3fc8439e05e9045d0d58ac07081b989d05d6d4af
Jeremy Rocheleau <107440471+slamdunk061@users.noreply.github.com>
2024-02-25 17:26:58 -0500

    Update hercules.sql
    Displays all employees whom first name is "Hercules" and last name starts with "B"

--- hercules.sql
+++ hercules.sql
@@ -1,1 +1,5 @@
 /* List all employees whose first name is "Hercules" and last names begin with "B." */
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules' AND last_name like 'B%';
